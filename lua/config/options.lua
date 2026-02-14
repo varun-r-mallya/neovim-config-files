afd82ec4c02df970f57bf5bba1388aa6565146e4
Varun R Mallya <varunrmallya@gmail.com>
2026-02-14 14:41:18 +0530

    :make kernel required changes
    Signed-off-by: Varun R Mallya <varunrmallya@gmail.com>

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,3 +2,10 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 vim.g.autoformat = false
+
+vim.opt.tabstop = 8
+vim.opt.softtabstop = 8
+vim.opt.shiftwidth = 8
+vim.opt.expandtab = false
+vim.opt.title = true
+
